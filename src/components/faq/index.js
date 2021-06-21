9908d613b536f00b6cf60a07598dfa0c797d84da
Ammar Qureshi <qureshiaq2003@gmail.com>
2021-06-22 00:10:58 +0500

    Push

---
+++ src/components/faq/index.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import Faq from "react-faq-component";
+
+const data = {
+  //   title: "FREQUENTLY ASKED QUESTIONS",
+  rows: [
+    {
+      title: "Who can rent a car?",
+      content:
+        "The car with driver can be rented by anyone above the age of 16. However, in case of self-drive, minimum age for hiring a car is 21 years and your license needs to be at least 1 year old. If you are a resident of Pakistan, you need to have a valid driving license along with NIC. If you are on a tourist visa then you need to have a valid home country license OR an international driving license and valid passport with visa stamp.",
+    },
+    {
+      title: "Can I rent a car with and without driver both?",
+      content:
+        "Yes, you may book any car available on our website, with driver. However, the self-drive option is currently limited to Suzuki Wagon R and Daihatsu Mira.",
+    },
+    {
+      title: "How can I pay the rent?",
+      content: "We accept cash and bank transfers at the moment.",
+    },
+    {
+      title: "Do I need to pay any security deposit?",
+      content:
+        "Yes, in case of the self-drive option, a security deposit is required.",
+    },
+    {
+      title: "What happens if an accident occurs?",
+      content:
+        "In case of Self-Driver option, we cover the small damages from your security deposit. Since all our cars are insured, the bigger damages are covered by the insurance. However, the debt charged by the insurance company shall be paid by the customer. Furthermore, if the car stays at the workshop for repairs / damage caused by the accident, the rent for that period shall also be paid by the customer.",
+    },
+    {
+      title: "What is your fuel policy?",
+      content:
+        "Customers are responsible for paying for their own fuel consumption. Each car is provided with a level of fuel and customers have to return the vehicle with the same level of fuel as they were given. We also provide an option to the customers, where they can pay for the number of kilometers done by the car at the specified rates for each car category, starting from the point the car leaves the office / Car Owner’s / Driver’s location.",
+    },
+  ],
+};
+
+const styles = {
+  bgColor: "white",
+  titleTextColor: "darkblue",
+  rowTitleColor: "darkblue",
+  rowContentColor: "black",
+  arrowColor: "darkblue",
+  
+};
+
+const config = {
+  //   animate: true,
+  //   arrowIcon: "V",
+  //   tabFocus: true
+};
+
+export default function FAq() {
+  return (
+    <div>
+      <Faq data={data} styles={styles} config={config} />
+    </div>
+  );
+}
--- src/screens/landing/index.js
+++ src/screens/landing/index.js
@@ -1,67 +1,105 @@
 import React from "react";
-import './style.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from '../../components/navbar'
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "../../components/navbar";
 import { Col, Container, Row } from "react-bootstrap";
-import Sedan from '../../assets/sedan.png'
-import Hatchback from '../../assets/hatch.png'
-import Luxury from '../../assets/luxury.png'
-import Suv from '../../assets/suv.png'
-import Vans from '../../assets/vans.png'
-
-
+import Sedan from "../../assets/sedan.png";
+import Hatchback from "../../assets/hatch.png";
+import Luxury from "../../assets/luxury.png";
+import Suv from "../../assets/suv.png";
+import Vans from "../../assets/vans.png";
+import FAq from "../../components/faq"; 
 
 export default function Landing() {
   return (
     <>
-      <NavBar/>
-      
-      <div className='bg'>
-      
-      </div>
-      <Container className='fullPageContainer'>
-        <Row style={{marginTop:"50px"}}>
+      <NavBar />
+
+      <div className="bg"></div>
+      <Container className="fullPageContainer">
+        <Row style={{ marginTop: "50px" }}>
           <Col>
-          <h1 style={{color:"darkblue"}}>What type of Car you are looking for?</h1>
+            <h1 style={{ color: "darkblue" }}>
+              What type of Car you are looking for?
+            </h1>
           </Col>
         </Row>
-      <Row className='carsRow'>
-        <Col className='carsContainer'>
-         <img className='pic' src={Sedan}/>
-         <h4 className='carsCategories'>Sedan</h4>
+        <Row className="carsRow">
+          <Col className="carsContainer">
+            <img className="pic" src={Sedan} />
+            <h4 className="carsCategories">Sedan</h4>
+          </Col>
+          <Col>
+            <img className="pic" src={Hatchback} />
+            <h4 className="carsCategories">Hatchback</h4>
+          </Col>
+          <Col>
+            <img className="pic" src={Luxury} />
+            <h4 className="carsCategories">Luxury</h4>
+          </Col>
+          <Col>
+            <img className="pic" src={Suv} />
+            <h4 className="carsCategories">Suv</h4>
+          </Col>
+          <Col>
+            <img className="pic2" src={Vans} />
+            <h4 className="carsCategories">Vans</h4>
+          </Col>
+        </Row>
 
+        <Row>
+          <Col
+            style={{
+              backgroundColor: "darkblue",
+              height: "200px",
+              borderRadius: "20px",
+              marginTop: "90px",
+            }}
+          >
+            <h2 style={{ color: "white", marginTop: "12px" }}>FOR-BOOKING</h2>
 
-        </Col>
-        <Col>
-         <img className='pic' src={Hatchback}/>
-         <h4 className='carsCategories'>Hatchback</h4>
+            <Col style={{ marginTop: "19px" }}>
+              <input
+                placeholder="Enter Pick-Up Location*"
+                className="form-control"
+                type="text"
+                style={{ marginTop: "12px" }}
+              />
+            </Col>
+            <Col style={{ display: "flex" }}>
+              <input className="date-input" type="date" />
+              <input value="Form Date" className="date-input" type="time" />
+              <Col style={{ backgroundColor: "white" }}>
+                <input
+                  style={{ height: "17px", width: "30px", marginTop: "12px" }}
+                  type="radio"
+                />
+                <label> FULL DAY</label>
+                <input style={{ height: "17px", width: "30px" }} type="radio" />
+                <label> SHORT RENTAL</label>
+              </Col>
+            </Col>
+            <Col>
+              <button className="bkn-btn">BOOK NOW</button>
+            </Col>
+          </Col>
+        </Row>
 
-
-        </Col>
-        <Col>
-         <img className='pic' src={Luxury}/>
-         <h4 className='carsCategories'>Luxury</h4>
-
-
-        </Col>
-        <Col>
-         <img className='pic' src={Suv}/>
-         <h4 className='carsCategories'>Suv</h4>
-
-
-        </Col>
-        <Col>
-         <img className='pic2' src={Vans}/>
-         <h4 className='carsCategories'>Vans</h4>
-
-
-        </Col>
-      </Row>
-
-      <Row style={{marginTop:"20px"}}>
-
-      </Row>
+        <Row style={{ marginTop: "20px" }}></Row>
       </Container>
+      <Row className="line"> </Row>
+      <Row>
+        <Col>
+          <h1 style={{ color: "darkblue",marginTop:"80px" }}>FREQUENTLY ASKED QUESTIONS</h1>
+        </Col>
+        </Row>
+        <Row>
+        
+      <Col>
+        <FAq/>
+       </Col>
+        
+      </Row>
     </>
   );
 }
